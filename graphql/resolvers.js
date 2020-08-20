50351b425ea8a7a6b1777dfaaa70e2096e1a97ab
Vichea Nath <48352653+vicheanath@users.noreply.github.com>
2020-08-21 00:04:46 +0700

    add database migration & resgister validator || part 3

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,58 @@
-module.export = {
+const bcrypt = require('bcryptjs')
+const { UserInputError } = require('apollo-server')
+const { User } = require('../models')
+
+module.exports = {
     Query: {
-        getUsers: () => {
-            const users = [
-                {
-                    username: "vichea",
-                    email: "vichea@gmail.com",
-                },
-                {
-                    username: "vichea2",
-                    email: "vichea2@gmail.com",
-                },
-            ];
-            return users
+        getUsers: async () => {
+            try {
+                const users = await User.findAll()
+
+                return users
+            } catch (err) {
+                console.log(err)
+            }
         },
     },
-};
+    Mutation: {
+        register: async (_, args) => {
+            let { username, email, password, confirmPassword } = args
+            let errors = {}
+            try {
+                // Validate input data
+                if (email.trim() === '') errors.email = "email must be empty"
+                if (username.trim() === '') errors.username = "username must be empty"
+                if (password.trim() === '') errors.password = "password must be empty"
+                if (confirmPassword.trim() === '') errors.confirmPassword = "repeat password must be empty"
+                if (password != confirmPassword) errors.confirmPassword = "password must mutch"
+                // Cheack If username / email exit
+                // const userByUsername = await User.findOne({ where: { username } })
+                // const userByEmail = await User.findOne({ where: { email } })
+
+                // if (userByUsername) errors.username = "Username already taken"
+                // if (userByEmail) errors.email = "Email already signup plese go to login page"
+                if (Object.keys(errors).length > 0) {
+                    throw errors
+                }
+
+
+                // Has Pass
+                password = await bcrypt.hash(password, 6)
+
+                //  Create user
+                const user = await User.create({
+                    username, email, password
+                })
+                //  Return User
+                return user
+            } catch (err) {
+                // console.log(err.errors)
+                console.log('**********************************')
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach((e) => errors[e.path] = `${e.path} is aleady taken`)
+                }
+                throw new UserInputError('Bad input', { errors })
+            }
+        }
+    }
+}
